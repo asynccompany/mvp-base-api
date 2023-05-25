338919a3f9505de3bf05a6bd0d7db64600b35809
C1325301 Kevin da Silva Ferreira <C1325301@interno.bb.com.br>
2023-05-25 11:25:20 -0300

    refactor: remove modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [UsersModule, RolesModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
